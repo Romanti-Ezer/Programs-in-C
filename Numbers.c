743cf762f078e267ff0341e62824cd3c2387181c
Romanti-Ezer <romanti.santos@fatec.sp.gov.br>
2017-09-14 16:25:23 -0300

    Update in README and code syntax translations

--- Numbers.c
+++ Numbers.c
@@ -4,7 +4,7 @@
 int main() {
    int i, amt_num = 0, amt_pair = 0, amt_odd, num_read;
    float sum_total = 0, num_avg, biggest_num, smaller_num, pair_avg, sum_pair = 0;
-   float porcent_odd;
+   float percent_odd;
    char status = 'Y';
    
    while ((status != 'N') && (status != 'n')){
@@ -41,17 +41,17 @@ int main() {
    num_avg = sum_total/amt_num;
    pair_avg = sum_pair/amt_pair;
    amt_odd = amt_num - amt_pair;
-   porcent_odd = amt_odd * 100 / amt_num;
+   percent_odd = amt_odd * 100 / amt_num;
    
    printf("\n\nSum of all numbers informed: %.2f\n", sum_total);
-   printf("Quantity of numbers informed: %d\n", amt_num);
+   printf("Amount of numbers informed: %d\n", amt_num);
    printf("Numbers' value average: %.2f\n", num_avg);
    printf("Biggest number: %.2f\n", biggest_num);
    printf("Smaller number: %.2f\n", smaller_num);
    
-   printf("\nPair numbers quantity: %d\n", amt_pair);
+   printf("\nPair numbers amount: %d\n", amt_pair);
    printf("Pair numbers value average: %.2f\n", pair_avg);
-   printf("\nOdd numbers porcent: %.2f\n", porcent_odd);
+   printf("\nOdd numbers percent: %.2f\n", percent_odd);
    printf("\nOdd numbers quantity: %d\n", amt_odd);
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Programs-in-C
 Programs created in C at the college
+
+# Numbers.c
+
+According to inputted numbers, shows:
+- The Sum of the informed numbers
+- The amount of informed numbers
+- The numbers' value average
+- The biggest value informed
+- The smaller number
+- The quantity and value average of pair numbers
+- The percent of odd numbers informed
